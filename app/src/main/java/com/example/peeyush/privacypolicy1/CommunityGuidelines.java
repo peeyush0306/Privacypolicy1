ff08978926bb1bb7b5a5fa31d94b684f3e5a19a7
Peeyush Singhal <peeyush_singhal03@yahoo.com>
2016-11-20 17:29:06 +0530

    initial commit

---
+++ app/src/main/java/com/example/peeyush/privacypolicy1/CommunityGuidelines.java
@@ -0,0 +1,13 @@
+package com.example.peeyush.privacypolicy1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CommunityGuidelines extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community_guidelines);
+    }
+}
